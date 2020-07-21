5826d147a95b4b6fddf25e0b048d80e9bd6b410b
Sergii Fil <manaxyd@gmail.com>
2020-07-21 23:47:22 +0300

    delete all commit

--- resources/views/include/auth.blade.php
+++
@@ -1,8 +0,0 @@
-@auth
-    <li><span>Добро пожаловать, {{ $auth->name }}</span></li>
-    <li><a href="/my/account">Аккаунт</a></li>
-    <li><a href="/logout">Выйти</a></li>
-@endauth
-@guest
-    <li><a href="/login">Войти</a></li>
-@endguest
